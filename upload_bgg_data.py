57ffcda0e0c90c27952f10081a0c37f776e38ad8
Robin Zigmond <robinzig@hotmail.com>
2017-06-03 11:37:02 +0100

    small changes to data upload script, README updated

--- upload_bgg_data.py
+++ upload_bgg_data.py
@@ -4,8 +4,8 @@ import boardgamegeek
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-HUNDREDS = 100  # constant for the number of id lists to get, each
-                # consisting of 10 games. 
+PAGES = 100  # constant for the number of pages of search results to look
+             # through. Each page consists of 100 items. 
 
 
 def get_game_ids(sortcriterion, firstpage, pages, sortdirection="desc"):
@@ -32,29 +32,37 @@ def get_game_ids(sortcriterion, firstpage, pages, sortdirection="desc"):
         html = page.read()
         soup = BeautifulSoup(html, "lxml")
         # now search for all boardgame links in ranking list
-        link_tags = soup.findAll(a_inside_td_with_class_collection_thumbnail)
+        link_tags = soup.findAll(is_link_in_ranking_list)
         # iterate through the list to get the id numbers.
         # (The id immediately follows "/boardgame/" in the href value)
         for link in link_tags:
-            # a few games have ids as both a standalone game and as an expansion -
-            # the same id in each case. But for some (all?) of these, the search
-            # page gives the link with the expansion form, so we need to handle
-            # both cases
-            if link["href"][:20] == "/boardgameexpansion/":
-                id_and_more = link["href"][20:]  # chop off the "/boardgameexpansion/"
-            else:
+            # many items in the search results are not "actual" games -
+            # they are expansions, or even something else. I am not interested
+            # in these "others" for this project.
+            if link["href"][:11] == "/boardgame/":
                 id_and_more = link["href"][11:]  # chop off the "/boardgame/"
-            slash_index = id_and_more.find("/")
-            new_id = int(id_and_more[:slash_index])
-            game_ids.append(new_id)
+                slash_index = id_and_more.find("/")
+                new_id = int(id_and_more[:slash_index])
+                game_ids.append(new_id)
+            else:
+                # if not an "actual" game, skip it
+                continue
+        print "%s actual games found on page" % len(game_ids)
     return game_ids
 
 
-def a_inside_td_with_class_collection_thumbnail(tag):
+def is_link_in_ranking_list(tag):
+    """
+    finds game links for items in the ranking list. From studying the html
+    for these pages, the distinguishing feature of them is that they are
+    links inside a <td>, and that <td> has a class of "collection_thumbnail"
+    """
     parent = tag.parent
     return (tag.name == "a"
             and parent.name == "td"
             and parent.has_attr("class")
+            # need to use a list for class names, because there can be multiple
+            # classes on a single element
             and parent["class"] == ["collection_thumbnail"]
            )
 
@@ -81,31 +89,29 @@ def update_game_database(bgg_client, id_lists):
     introduce many duplicates and make the database grow huge over time).
     """
     # create connection:
-    client = MongoClient(host="localhost", port=27017)
-    db = client.BGG
-    collection = db.game_info
-    # first wipe previous data
-    collection.delete_many({})
-    counter = 1
-    for list in id_lists:
-        time.sleep(10)
-        print "trying to upload API data for hundred no. %s of %s" % (counter, HUNDREDS)
-        # first get game data from API. The following API call returns a
-        # list of objects representing the games: 
-        games = bgg_client.game_list(game_id_list=list)
-        # for each game, the .data() method returns a dictionary of the
-        # game's data. We pack those dictionaries into a list
-        data_dict = map(get_good_data, games)
-        # upload new data
-        collection.insert_many(data_dict)
-        print "successfully uploaded to MongoDB!"
-        counter = counter+1
+    with MongoClient(host="localhost", port=27017) as client:
+        db = client.BGG
+        collection = db.game_info
+        # first wipe previous data
+        collection.delete_many({})
+        counter = 0
+        for list in id_lists:
+            time.sleep(5)  # pause to prevent API throttling
+            counter = counter+1
+            print "trying to upload API data for page %s of %s" % (counter, PAGES)
+            # first get game data from API. The following API call returns a
+            # list of objects representing the games: 
+            games = bgg_client.game_list(game_id_list=list)
+            # for each game, the .data() method returns a dictionary of the
+            # game's data. We pack those dictionaries into a list
+            data_dict = map(get_good_data, games)
+            # upload new data
+            collection.insert_many(data_dict)
+            print "successfully uploaded to MongoDB!"
 
 
-bgg = boardgamegeek.BGGClient(requests_per_minute=10)
-# the default requests_per_minute is 30, but in practice this seems too fast
-# to be allowed access to all data needed.
+bgg = boardgamegeek.BGGClient()
 id_lists = []
-for i in range (0, HUNDREDS):
+for i in range (0, PAGES):
     id_lists.append(get_game_ids("numvoters", i+1, 1))
 update_game_database(bgg, id_lists)  # upload data to Mongo
