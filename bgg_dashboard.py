5eb1de56ae7c88817ce76c48c16507d65fe02858
Robin Zigmond <robinzig@hotmail.com>
2017-06-26 00:29:58 +0100

    connection to MongoDB

--- bgg_dashboard.py
+++ bgg_dashboard.py
@@ -1,12 +1,12 @@
+import json
+import os
 from flask import Flask, render_template
 from pymongo import MongoClient
-import json
 
 app = Flask(__name__)
 
-MONGODB_HOST = "localhost"
-MONGODB_PORT = 27017
-DBS_NAME = "BGG"
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'BGG')
 COLLECTION_NAME = "game_info"
 
 @app.route("/")
@@ -48,7 +48,7 @@ def game_data():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
